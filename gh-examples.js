9d79748342b804e579eded171b7240210a97135b
suhorng <suhorng@gmail.com>
2014-03-13 23:34:08 +0800

    tidy up ghscript examples

--- gh-examples.js
+++ gh-examples.js
@@ -1,14 +1,4 @@
-/*
-https://github.com/ajaxorg/node-github
-https://github.com/ajaxorg/node-github/blob/master/api/v3.0.0/authorizationTest.js
-https://github.com/ajaxorg/node-github/blob/master/api/v3.0.0/orgsTest.js
-
-??? https://github.com/darvin/github
-??? https://www.npmjs.org/package/github-api 
-*/
-
 var fs = require("fs");
-
 var _ = require("underscore");
 
 var ghapi = require("github");
@@ -19,69 +9,72 @@ gh.authenticate({
   token: fs.readFileSync("../my-oauth-token", { encoding: "utf-8" })
 });
 
-/*
+======== SYNTAX ERROR HERE =======
+NOTE: THIS CODE IS NOT FOR RUNNING
+==================================
+
+// list all members
 gh.orgs.getMembers({ org: "NTUDSA2014"}, function(err, data) {
   console.log(data);
 });
-*/
 
-/*
-console.log("getting teams..");
+
+// list all teams
 gh.orgs.getTeams({ "org": "NTUDSA2014" }, function(err, data) {
   console.log("err: " + err);
   console.log(data);
 });
-*/
 
-/*
+
+// given a team, list its repositories
 console.log("getting repos..");
-gh.orgs.getTeamRepos({ "id": 717222 }, function(err, data) {
-  console.log("err: " + err);
-  _.each(data, function(datum) {
-    console.log({ "id": datum.id
-                , "name": datum.name,
-                "owner": datum.owner,
-                "pushed_at": datum.pushed_at });
-  });
+gh.orgs.getTeamRepos({ "id": 717222 }, // team id, e.g. `test-team2`
+  function(err, data) {
+    console.log("err: " + err);
+    _.each(data, function(repo) {
+      console.log({ "id": repo.id
+                  , "name": repo.name
+                  , "owner": repo.owner
+                  , "pushed_at": repo.pushed_at }); // last-pushed-at, GMT time
+    });
 });
-*/
 
-/*
-gh.orgs.addTeamRepo({ "id": 717204 // team id
+
+// add a repository to a team
+gh.orgs.addTeamRepo({ "id": 717204 // team id, e.g. `test-team`
                     , "user": "suhorng"
                     , "repo": "private-repo2" }
                     , function(err, data) {
   console.log("err: " + err);
   console.log(data);
 });
-*/
 
-/*
-gh.orgs.deleteTeamRepo({ "id": 717204 // team id
+
+// remove a repository from a team
+gh.orgs.deleteTeamRepo({ "id": 717204 // team id, e.g. `test-team`
                        , "user": "suhorng"
                        , "repo": "private-repo2" }
                        , function(err, data) {
   console.log("err: " + err);
   console.log(data);
 });
-*/
 
-/*
+
+// create a new team
 gh.orgs.createTeam({ "org": "NTUDSA2014"
                    , "name": "nodetest2"
                    , "repo_names": ["NTUDSA2014/private-repo2"]
                    , "permission": "pull" }
                    , function(err, data) {
   console.log("err: " + err);
-  console.log(data); // data.id: team id; 730680 for `nodetest2`
+  console.log(data); // data.id: team id;
 });
-*/
 
-/*
+
+// add someone to a team
 gh.orgs.addTeamMember({ "id": 730693 // team id, e.g. `nodetest2`
                       , "user": "suhorng-" }
                       , function(err, data) {
   console.log("err: " + err);
   console.log(data);
 });
-*/
