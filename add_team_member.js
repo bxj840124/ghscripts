fccdf71778498beae642b0c547faf2f6252aaaef
suhorng <suhorng@gmail.com>
2014-03-18 03:04:18 +0800

    add scripts for creating teams

---
+++ add_team_member.js
@@ -0,0 +1,72 @@
+var fs = require("fs");
+var und = require("underscore");
+
+var ghapi = require("github");
+var gh = new ghapi({ version: "3.0.0" });
+
+gh.authenticate({
+  type: "oauth",
+  token: fs.readFileSync("../my-oauth-token", { encoding: "utf-8" })
+});
+
+var names =
+  JSON.parse(
+    fs.readFileSync("../jsons/stugit.json"
+                   , { encoding: "utf-8" }))).map(function (stu) {
+  return { stu_id: stu.stu_id.toLowerCase(), github_name: stu.github_name };
+});
+
+var teams =
+  JSON.parse(
+    fs.readFileSync("../jsons/team_data.json", { encoding: "utf-8" }));
+
+var team_id = new Array();
+
+und.each(teams, function (team) {
+  team_id[team.team_data.name] = team.team_data.id;
+});
+
+// forM :: [a] -> (a -> Cont r b) -> Cont r [b]
+function forM(arr, fn, k) {
+  res = new Array();
+  (function rec(i) {
+    if (i < arr.length) {
+      fn(arr[i], function(data) {
+        res.push(data);
+        rec(i+1);
+      });
+    } else {
+      k(res);
+    }
+  })(0);
+}
+
+var add_error = new Array(),
+    add_ok = new Array();
+
+forM(names, function(usr, k) {
+  console.log("Adding " + usr.github_name + " to " + team_id[usr.stu_id]);
+  if (team_id[usr.stu_id] === undefined) {
+    console.log("undefined ERROR");
+    add_error.push({ "student": usr, "error": "undefined" });
+    k(false);
+  } else {
+    gh.orgs.addTeamMember({ "id": team_id[usr.stu_id]
+                          , "user": usr.github_name }
+                          , function(err, data) {
+      if (err !== null) {
+        console.log("ERROR");
+        add_error.push({ "student": usr, "error": err });
+        k(false);
+      } else {
+        add_ok.push({ "student": usr, "data": data });
+        k(data);
+      }
+    });
+  }
+}, function(res) {
+
+fs.writeFileSync("team_add_ok.json", JSON.stringify(add_ok, null, 2));
+fs.writeFileSync("team_add_error.json", JSON.stringify(add_error, null, 2));
+console.log("finish: total " + add_error.length + " error(s).");
+});
---
+++ create_teams.js
@@ -0,0 +1,59 @@
+var fs = require("fs");
+var und = require("underscore");
+
+var ghapi = require("github");
+var gh = new ghapi({ version: "3.0.0" });
+
+gh.authenticate({
+  type: "oauth",
+  token: fs.readFileSync("../my-oauth-token", { encoding: "utf-8" })
+});
+
+var stus = JSON.parse(fs.readFileSync("../jsons/stulist.json"));
+
+// forM :: [a] -> (a -> Cont r b) -> Cont r [b]
+function forM(arr, fn, k) {
+  res = new Array();
+  (function rec(i) {
+    if (i < arr.length) {
+      fn(arr[i], function(data) {
+        res.push(data);
+        rec(i+1);
+      });
+    } else {
+      k(res);
+    }
+  })(0);
+}
+
+var repos = ["NTUDSA2014/dsa14hw3", "NTUDSA2014/dsa14hw4", "NTUDSA2014/dsa14hw5",
+             "NTUDSA2014/dsa14hw6", "NTUDSA2014/dsa14hw7"]
+
+var create_errors = new Array(),
+    ret_data = new Array();
+
+forM(stus, function(stu, k) {
+  if (stu.identity == "校內生") {
+    console.log("Creating team " + stu.stu_id);
+    gh.orgs.createTeam({ "org": "NTUDSA2014"
+                       , "name": stu.stu_id
+                       , "repo_names": repos
+                       , "permission": "pull" }
+                       , function(err, data) {
+      if (err !== null) {
+        console.log("ERROR");
+        create_errors.push({ "student": stu, "error": err});
+      } else {
+        ret_data.push({ "student": stu, "team_data": data });
+      }
+      k(data);
+    });
+  } else {
+    k(false);
+  }
+}, function(res) {
+
+fs.writeFileSync("create_error.json", JSON.stringify(create_errors, null, 2));
+fs.writeFileSync("team_data.json", JSON.stringify(ret_data, null, 2));
+console.log("done: total " + create_errors.length + "error(s).");
+});
